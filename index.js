3512e0f813aef6a9025b47c8defd9434a15c3c36
leopoldkwok <leopoldkwok@gmail.com>
2020-08-19 06:32:14 +0100

    Picking out elements

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// javascript
+const grid = document.querySelector('.grid');
+const startButton = document.getElementById('start');
+const score = document.getElementById('score');
+
+function createGrid() {
+  // create elements
+  // create 100 of these elements
+  // add styling to these elements
+  // create array of squares
+}
