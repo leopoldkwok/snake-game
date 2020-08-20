2fb989a7805a3f440dae909ab6e92db6171f096e
leopoldkwok <leopoldkwok@gmail.com>
2020-08-20 06:40:42 +0100

    Changing Snake's direction

--- index.pack.js
+++ index.pack.js
@@ -79,6 +79,7 @@ var score = document.getElementById('score');
 var squares = [];
 var currentSnake = [2, 1, 0];
 var direction = 1;
+var width = 10;
 
 function createGrid() {
   // create 100 of these elements with a for loop
@@ -123,15 +124,19 @@ var timerId = setInterval(move, 1000);
 function control(e) {
   if (e.keyCode === 39) {
     console.log('right pressed');
+    direction = 1;
   } else if (e.keyCode === 38) {
     console.log('up pressed');
+    direction = -width;
   } else if (e.keyCode === 37) {
     console.log('left pressed');
+    direction = -1;
   } else if (e.keyCode === 40) {
     console.log('down pressed');
+    direction = +width;
   }
 }
-// document.addEventListener('keyup', control);
+document.addEventListener('keyup', control);
 
 /***/ })
 /******/ ]);
