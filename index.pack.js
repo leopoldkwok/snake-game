5e5ac90cfa130534ea932f3fc813adbf92044909
leopoldkwok <leopoldkwok@gmail.com>
2020-08-23 10:10:52 +0100

    Implementing functionality to our Start Button

--- index.pack.js
+++ index.pack.js
@@ -84,80 +84,79 @@ var appleIndex = 0;
 var score = 0;
 var intervalTime = 1000;
 var speed = 0.9;
+var timerId = 0;
 
 function createGrid() {
-  // create 100 of these elements with a for loop
+  //create 100 of these elements with a for loop
   for (var i = 0; i < width * width; i++) {
-    // create element
+    //create element
     var square = document.createElement('div');
-    // add styling to these element
+    //add styling to the element
     square.classList.add('square');
-    // put the element into our grid
+    //put the element into our grid
     grid.appendChild(square);
-    // push it into a new square array
+    //push it into a new squares array
     squares.push(square);
   }
 }
-
 createGrid();
 
 currentSnake.forEach(function (index) {
   return squares[index].classList.add('snake');
 });
 
+function startGame() {
+  timerId = setInterval(move, intervalTime);
+}
+
 function move() {
   if (currentSnake[0] + width >= width * width && direction === width || //if snake has hit bottom
-  currentSnake[0] % width === width - 1 && direction === 1 || // if snake has hit right wall
-  currentSnake[0] % width === 0 && direction === -1 || // if snake has hit left wall
-  currentSnake[0] - width < 0 && direction === -width || // if snake has hit top
+  currentSnake[0] % width === width - 1 && direction === 1 || //if snake has hit right wall
+  currentSnake[0] % width === 0 && direction === -1 || //if snake has hit left wall
+  currentSnake[0] - width < 0 && direction === -width || //if snake has hit top
   squares[currentSnake[0] + direction].classList.contains('snake')) return clearInterval(timerId);
 
-  // remove last element from our currentSnake array
+  //remove last element from our currentSnake array
   var tail = currentSnake.pop();
-  // remove styling from last element
+  //remove styling from last element
   squares[tail].classList.remove('snake');
-  // add square in direction we are heading
+  //add square in direction we are heading
   currentSnake.unshift(currentSnake[0] + direction);
+  //add styling so we can see it
 
-  // deal with snake head getting the apple
+  //deal with snake head gets apple
   if (squares[currentSnake[0]].classList.contains('apple')) {
     //remove the class of apple
     squares[currentSnake[0]].classList.remove('apple');
-    // grow our snake by adding class of snake to it
+    //grow our snake by adding class of snake to it
     squares[tail].classList.add('snake');
     console.log(tail);
-    // grow our snake array
+    //grow our snake array
     currentSnake.push(tail);
     console.log(currentSnake);
-    // generate a new apple
+    //generate new apple
     generateApple();
-    // add one to the score
+    //add one to the score
     score++;
-    // display our score
+    //display our score
     scoreDisplay.textContent = score;
-    // speed up our snake
+    //speed up our snake
     clearInterval(timerId);
     console.log(intervalTime);
     intervalTime = intervalTime * speed;
     console.log(intervalTime);
     timerId = setInterval(move, intervalTime);
   }
-  // add styling so we can see it
+
   squares[currentSnake[0]].classList.add('snake');
 }
 
-move();
-
-var timerId = setInterval(move, intervalTime);
-
 function generateApple() {
   do {
-    // generate a random number
     appleIndex = Math.floor(Math.random() * squares.length);
   } while (squares[appleIndex].classList.contains('snake'));
   squares[appleIndex].classList.add('apple');
 }
-
 generateApple();
 
 // 39 is right arrow
@@ -180,7 +179,8 @@ function control(e) {
     direction = +width;
   }
 }
-document.addEventListener('keydown', control);
+document.addEventListener('keyup', control);
+startButton.addEventListener('click', startGame);
 
 /***/ })
 /******/ ]);
